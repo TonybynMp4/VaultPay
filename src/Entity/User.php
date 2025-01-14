f1d5fcbe22b31627f5060d0680fed761a3666d40
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-01-14 15:41:15 +0100

    Refactor modify user for auth

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,107 +5,178 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_UUID', fields: ['uuid'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $Name = null;
+    #[ORM\Column(length: 180)]
+    private ?string $uuid = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $Surname = null;
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
-    private ?\DateTimeInterface $Birthdate = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $Email = null;
+    private ?\DateTimeInterface $birthdate = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $Password = null;
+    private ?string $email = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    private ?string $Photo = null;
+    private ?string $photo = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getUuid(): ?string
     {
-        return $this->Name;
+        return $this->uuid;
     }
 
-    public function setName(string $Name): static
+    public function setUuid(string $uuid): static
     {
-        $this->Name = $Name;
+        $this->uuid = $uuid;
 
         return $this;
     }
 
-    public function getSurname(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->Surname;
+        return (string) $this->uuid;
     }
 
-    public function setSurname(string $Surname): static
+    /**
+     * @see UserInterface
+     * @return list<string>
+     */
+    public function getRoles(): array
     {
-        $this->Surname = $Surname;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
 
         return $this;
     }
 
     public function getBirthdate(): ?\DateTimeInterface
     {
-        return $this->Birthdate;
+        return $this->birthdate;
     }
 
-    public function setBirthdate(\DateTimeInterface $Birthdate): static
+    public function setBirthdate(\DateTimeInterface $birthdate): static
     {
-        $this->Birthdate = $Birthdate;
+        $this->birthdate = $birthdate;
 
         return $this;
     }
 
     public function getEmail(): ?string
     {
-        return $this->Email;
+        return $this->email;
     }
 
-    public function setEmail(string $Email): static
+    public function setEmail(string $email): static
     {
-        $this->Email = $Email;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->Password;
-    }
-
-    public function setPassword(string $Password): static
-    {
-        $this->Password = $Password;
+        $this->email = $email;
 
         return $this;
     }
 
     public function getPhoto(): ?string
     {
-        return $this->Photo;
+        return $this->photo;
     }
 
-    public function setPhoto(?string $Photo): static
+    public function setPhoto(?string $photo): static
     {
-        $this->Photo = $Photo;
+        $this->photo = $photo;
 
         return $this;
     }
