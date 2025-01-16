0c74d7d2701395e7613993d12cf93d3b9dce4111
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-01-16 18:22:42 +0100

    refactor: remove console logs from header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,9 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const openButton = document.getElementById('nav_open');
     const asideNav = document.getElementById('aside_nav');
 
-    console.log(openButton);
-    console.log(asideNav);
-
     openButton.addEventListener('click', function() {
         asideNav.style.display = asideNav.style.display === 'block' ? 'none' : 'block';
     });
