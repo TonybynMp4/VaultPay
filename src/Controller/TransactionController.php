af908ac3948c1613dd86c4977e8b661591bebf02
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-01-16 23:45:57 +0100

    feat: implement transaction validation and creation logic in TransactionRepository

--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use App\Entity\Transaction;
+use App\Entity\Users;
 use App\Form\DepositFormType;
 use App\Repository\TransactionRepository;
 
@@ -31,70 +32,24 @@ class TransactionController extends AbstractController
 
             $fromAccountId = $data['from_account_id'];
             $toAccountId = $data['to_account_id'];
-            $amount = $data['amount'];
-            $label = $data['label'] ?? null;
-            $type = 0;
-
             $fromAccount = $bankAccountRepository->find($fromAccountId);
             $toAccount = $bankAccountRepository->find($toAccountId);
 
-            $error = false;
 
-            if ($fromAccountId === $toAccountId) {
-                $this->addFlash('error', 'Les comptes source et destination doivent être différents.');
-                $error = true;
+            $newTransaction = new Transaction();
+            $newTransaction
+                ->setAmount($data['amount'])
+                ->setFromAccount($fromAccount)
+                ->setToAccount($toAccount)
+                ->setType(0);
+
+            $result = $entityManager->getRepository(Transaction::class)->createTransaction($newTransaction, $this->getUser());
+            if ($result === true) {
+                $this->addFlash('success', 'Le virement a été effectué avec succès');
+                return $this->redirectToRoute('app_dashboard');
+            } else {
+                $this->addFlash('error', $result);
             }
-
-            if ($amount <= 0) {
-                $this->addFlash('error', 'Le montant doit être positif.');
-                $error = true;
-            }
-
-            if (!$fromAccount || !$toAccount) {
-                $this->addFlash('error', 'Un des comptes est introuvable.');
-                $error = true;
-            }
-
-            if ($fromAccount->getUserId() !== $this->getUser()) {
-                $this->addFlash('error', 'Vous ne pouvez pas effectuer un virement depuis ce compte.');
-                $error = true;
-            }
-
-            if ($fromAccount->getBalance() < $amount) {
-                $this->addFlash('error', 'Solde insuffisant.');
-                $error = true;
-            } elseif ($fromAccount->isClose()) {
-                $this->addFlash('error', 'Le compte source est fermé.');
-                $error = true;
-            } elseif ($toAccount->isClose()) {
-                $this->addFlash('error', 'Le compte destinataire est fermé.');
-                $error = true;
-            }
-            if ($error)
-                return $this->redirectToRoute('transaction_transfer');
-
-            $fromAccount->setBalance($fromAccount->getBalance() - $amount);
-            $toAccount->setBalance($toAccount->getBalance() + $amount);
-
-            if (!$label) {
-                $label = 'Virement du compte ' . $fromAccount->getId() . ' au compte ' . $toAccount->getId();
-            }
-
-            // Enregistrer la transaction
-            $transaction = new Transaction();
-            $transaction->setFromAccount($fromAccount);
-            $transaction->setToAccount($toAccount);
-            $transaction->setAmount($amount);
-            $transaction->setType($type);
-            $transaction->setLabel($label);
-            $transaction->setDate(new \DateTime());
-            $transaction->setCancel(false);
-
-            $entityManager->persist($transaction);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Transaction effectuée avec succès.');
-            return $this->redirectToRoute('app_dashboard');
         }
 
         return $this->render('transaction/transfer.html.twig', [
@@ -115,32 +70,19 @@ class TransactionController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            $amount = $data->getAmount();
-            $toAccount = $data->getToAccount();
-            $label = $data->getLabel() ?? "Dépôt sur le compte " . $toAccount->getId();
 
-            // Validation des données
-            if ($amount <= 0) {
-                $this->addFlash('error', 'Le montant doit être positif.');
-            } else {
-                // Create new transaction
-                $transaction = new Transaction();
-                $transaction->setAmount($amount);
-                $transaction->setLabel($label);
-                $transaction->setType(2); // Deposit type
-                $transaction->setToAccount($toAccount);
-                $transaction->setDate(new \DateTime());
+            $newTransaction = new Transaction;
+            $newTransaction
+                ->setAmount($data['amount'])
+                ->setToAccount($data['to_account_id'])
+                ->setType(2);
 
-                // Update account balance
-                $newBalance = $toAccount->getBalance() + $amount;
-                $toAccount->setBalance($newBalance);
-
-                // Save changes
-                $entityManager->persist($transaction);
-                $entityManager->flush();
-
-                $this->addFlash('success', 'Le dépôt a été effectué avec succès.');
+            $result = $entityManager->getRepository(Transaction::class)->createTransaction($newTransaction);
+            if ($result === true) {
+                $this->addFlash('success', 'Le dépôt a été effectué avec succès');
                 return $this->redirectToRoute('app_account');
+            } else {
+                $this->addFlash('error', $result);
             }
         }
 
@@ -153,8 +95,7 @@ class TransactionController extends AbstractController
     #[Route('/transaction/withdraw', name: 'transaction_withdraw')]
     public function showRetrait(
         Request $request,
-        BankAccountRepository $bankAccountRepository,
-        EntityManagerInterface $entityManager
+        TransactionRepository $transactionRepository,
     ): Response {
         $form = $this->createForm(TransferFormType::class);
         $form->handleRequest($request);
@@ -162,53 +103,27 @@ class TransactionController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
 
-            $fromAccountId = $data['from_account_id'];
-            $amount = $data['amount'];
-            $label = $data['label'] ?? null;
-            $type = 2;
+            $newTransaction = new Transaction;
+            $newTransaction
+                ->setAmount($data['amount'])
+                ->setFromAccount($data['from_account_id'])
+                ->setType(1);
 
-            // Validation des données
-            if ($amount <= 0) {
-                $this->addFlash('error', 'Le montant doit être positif.');
+            $result = $transactionRepository->createTransaction($newTransaction);
+            if ($result === true) {
+                $this->addFlash('success', 'Le retrait a été effectué avec succès');
+                return $this->redirectToRoute('app_account');
             } else {
-                $fromAccount = $bankAccountRepository->find($fromAccountId);
-
-                if (!$fromAccount) {
-                    $this->addFlash('error', 'Le compte est introuvable.');
-                } elseif ($fromAccount->getBalance() < $amount) {
-                    $this->addFlash('error', 'Solde insuffisant.');
-                } elseif ($fromAccount->isClose()) {
-                    $this->addFlash('error', 'Le compte est fermé.');
-                } else {
-                    // Effectuer le retrait
-                    $fromAccount->setBalance($fromAccount->getBalance() - $amount);
-
-                    if (!$label) {
-                        $label = 'Retrait sur le compte ' . $fromAccount->getId();
-                    }
-
-                    // Enregistrer la transaction
-                    $transaction = new Transaction();
-                    $transaction->setFromAccount($fromAccount);
-                    $transaction->setAmount($amount);
-                    $transaction->setType($type);
-                    $transaction->setLabel($label);
-                    $transaction->setDate(new \DateTime());
-                    $transaction->setCancel(false);
-
-                    $entityManager->persist($transaction);
-                    $entityManager->flush();
-
-                    $this->addFlash('success', 'Transaction effectuée avec succès.');
-                }
+                $this->addFlash('error', $result);
             }
-            return $this->redirectToRoute('transaction_withdraw');
         }
 
         return $this->render('transaction/withdraw.html.twig', [
             'form' => $form->createView(),
         ]);
     }
+
+    #[IsGranted('ROLE_USER')]
     #[Route('/transaction/history', name: 'transaction_history')]
     public function showHistory(
         TransactionRepository $transactionRepository
@@ -216,16 +131,16 @@ class TransactionController extends AbstractController
         $user = $this->getUser();
         $accounts = $user->getBankAccounts();
         $transactions = [];
-        
+
         foreach ($accounts as $account) {
             $accountTransactions = $transactionRepository->findLastTransactionsByAccount($account->getId(), 100);
             $transactions = array_merge($transactions, $accountTransactions);
         }
-        
+
         usort($transactions, function ($a, $b) {
             return $b->getDate() <=> $a->getDate();
         });
-        
+
         return $this->render('transaction/history.html.twig', [
             'transactions' => $transactions,
         ]);
--- src/Repository/TransactionRepository.php
+++ src/Repository/TransactionRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repository;
 
+use App\Entity\BankAccount;
 use App\Entity\Transaction;
+use App\Entity\Users;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,7 +18,7 @@ class TransactionRepository extends ServiceEntityRepository
         parent::__construct($registry, Transaction::class);
     }
 
-    public function findLastTransactionsByAccount(int $accountId, int $limit = 5): array
+    public function findLastTransactionsByAccount(int $accountId, int $limit): array
     {
         return $this->createQueryBuilder('t')
             ->where('t.FromAccount = :accountId OR t.ToAccount = :accountId')
@@ -25,5 +27,105 @@ class TransactionRepository extends ServiceEntityRepository
             ->setMaxResults($limit)
             ->getQuery()
             ->getResult();
+    }
+
+    // Valide que le
+    public function validateTransaction(Transaction $transaction, Users $user): string
+    {
+        $error = "";
+
+        if ($transaction->getFromAccount() === $transaction->getToAccount()) {
+            $error = "Les comptes source et destination doivent être différents";
+        }
+
+        if (!$transaction->getType() === 0 && (!$transaction->getFromAccount() || !$transaction->getToAccount())) {
+            $error = "Un des comptes est introuvable";
+        }
+        if ($transaction->getType() === 1 && !$transaction->getFromAccount()) {
+            $error = "Le compte source est introuvable";
+        }
+
+        if ($transaction->getType() === 2 && !$transaction->getToAccount()) {
+            $error = "Le compte destination est introuvable";
+        }
+
+        if ($transaction->getFromAccount()->isClose() || $transaction->getToAccount()->isClose()) {
+            $error = "Un des comptes est fermé";
+        }
+
+        if ($transaction->getAmount() <= 0) {
+            $error = "Le montant doit être positif";
+        }
+
+        if ($transaction->getFromAccount() && $transaction->getFromAccount()->getUser()->getId() !== $user->getId()) {
+            $error = "Vous ne pouvez pas effectuer un virement depuis ce compte";
+        }
+
+        if ($transaction->getFromAccount() && $transaction->getFromAccount()->getBalance() < $transaction->getAmount()) {
+            $error = "Solde insuffisant";
+        }
+
+        return $error;
+    }
+
+    // retourne la transaction prête à être persistée si tout est ok, sinon une string d'erreur
+    public function createTransaction(Transaction $transaction, Users $user): string|bool
+    {
+        $validationError = $this->validateTransaction($transaction, $user);
+        if ($validationError !== "") {
+            return $validationError;
+        }
+
+        $label = $transaction->getLabel();
+
+        if (!$label) {
+            switch ($transaction->getType()) {
+                case 0:
+                    $label = 'Virement du compte ' . $transaction->getFromAccount()->getId() . ' au compte ' . $transaction->getToAccount()->getId();
+                    break;
+                case 1:
+                    $label = 'Débit du compte ' . $transaction->getFromAccount()->getId();
+                    break;
+                case 2:
+                    $label = 'Crédit du compte ' . $transaction->getToAccount()->getId();
+                    break;
+                default:
+                    throw new \Exception("Type de transaction inconnu");
+                    break;
+            }
+        }
+
+        $transaction->setLabel($label);
+        $transaction->setDate(new \DateTime());
+        $transaction->setCancel(false);
+
+        // Si un compte source est défini, on débite le montant
+        if ($transaction->getFromAccount()) {
+            $transaction->getFromAccount()->setBalance($transaction->getFromAccount()->getBalance() - $transaction->getAmount());
+        }
+
+        // Si un compte destination est défini, on crédite le montant
+        if ($transaction->getToAccount()) {
+            $transaction->getToAccount()->setBalance($transaction->getToAccount()->getBalance() + $transaction->getAmount());
+        }
+
+        $this->getEntityManager()->persist($transaction);
+        $this->getEntityManager()->flush();
+
+        return true;
+    }
+
+    // retourne true si la transaction a été annulée, false sinon
+    public function cancelTransaction($transactionId): bool
+    {
+        $transaction = $this->find($transactionId);
+        if ($transaction && !$transaction->isCancel()) {
+            $transaction->setCancel(true);
+            $this->getEntityManager()->persist($transaction);
+            $this->getEntityManager()->flush();
+            return true;
+        }
+
+        return false;
     }
 }
