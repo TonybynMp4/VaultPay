e78fb5cabacdce858653a09ac5ec0b2d385cbe72
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-01-16 01:04:49 +0100

    feat: account type selection

--- src/Form/BankAccountType.php
+++ src/Form/BankAccountType.php
@@ -3,10 +3,12 @@ namespace App\Form;
 
 use App\Entity\BankAccount;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 class BankAccountType extends AbstractType
@@ -17,9 +19,19 @@ class BankAccountType extends AbstractType
             ->add('name', TextType::class, [
                 'label' => 'Nom du compte',
             ])
+            ->add('type', ChoiceType::class, [
+                'label' => 'Type de compte',
+                'choices' => [
+                    'Courant' => 0,
+                    'Epargne' => 1,
+                ],
+            ])
             ->add('balance', MoneyType::class, [
                 'label' => 'Montant initial',
                 'currency' => 'EUR',
+                'constraints' => [
+                    new Assert\Positive(),
+                ],
             ]);
     }
 
