fb326ed3715276b99cbe3591d794d1ce9e55c749
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-01-17 09:41:29 +0100

    feat: update styles and enhance user feedback in dashboard and login flow

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\TransactionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -15,11 +16,17 @@ class HomeController extends AbstractController {
 
     #[IsGranted('ROLE_USER')]
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function dashboard(TransactionRepository $transactionRepository): \Symfony\Component\HttpFoundation\Response {
+    public function dashboard(Security $security, TransactionRepository $transactionRepository): \Symfony\Component\HttpFoundation\Response {
         $user = $this->getUser();
 
         if (!$user) {
             return $this->redirectToRoute('app_login');
+        }
+
+        if ($user->isBlocked()) {
+            $security->logout(false);
+            $this->addFlash('error', 'Votre compte est été bloqué.');
+            return $this->redirectToRoute('app_home');
         }
 
         $transactions = $transactionRepository->findLastTransactionsByUser($user, 10);
